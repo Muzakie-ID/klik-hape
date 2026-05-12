f215a2178ee5cb68a1d00baa662c08f770bb39f3
Muzakie <76565734+Muzakie-ID@users.noreply.github.com>
2026-05-12 19:25:27 +0700

    style: fix mobile padding on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,8 +5,8 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-6 sm:py-12">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
             <!-- 1. KARTU STATISTIK -->
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mb-8">
