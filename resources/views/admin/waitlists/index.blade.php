42ccadb67c7f7fa53172f76f8d03fdb374c9c8e1
Muzakie <76565734+Muzakie-ID@users.noreply.github.com>
2026-05-12 19:36:51 +0700

    style: fix mobile padding on waitlists page

--- resources/views/admin/waitlists/index.blade.php
+++ resources/views/admin/waitlists/index.blade.php
@@ -5,8 +5,8 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-6 sm:py-12">
+        <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
             <div class="mb-4 flex flex-col sm:flex-row gap-4 justify-between items-center">
                 <p class="text-sm text-gray-600">
